bab887b34cd71f4da9b7ee3671b4e5efddcf391d
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2026-09-11 13:34:01 -0500

    feat(aws): update client module

--- modules/aws/components/client/scripts/client.sh
+++ modules/aws/components/client/scripts/client.sh
@@ -43,4 +43,36 @@ asdf set -u python 3.12.13
 uv tool install https://github.com/mminichino/host-prep-lib/releases/download/${host_prep_version}/pyhostprep-${host_prep_version}-py3-none-any.whl
 uv tool install ansible-core --with ansible
 
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc docker-buildx podman-docker containerd runc | cut -f1)
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+UID_MIN=1000
+while IFS=: read -r username _ uid gid _ home shell; do
+    if [[ "$username" == "root" || "$uid" -eq 0 ]]; then
+        continue
+    fi
+
+    if [[ "$uid" -lt "$UID_MIN" ]]; then
+        continue
+    fi
+
+    if id -nG "$username" 2>/dev/null | tr ' ' '\n' | grep -qx "docker"; then
+        continue
+    fi
+
+    usermod -aG docker "$username"
+    echo "Added '$username' to docker group"
+done < /etc/passwd
+
 touch "$FLAG_FILE"
